69a9668696c16c3756a40dddcdcfe21ea0e30de4
MartinBournon <martinbournon@gmail.com>
2023-09-12 23:09:15 -0300

    eliminado import innecesario

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,7 +1,6 @@
 import { useState,useEffect } from "react"
 import { useParams } from "react-router-dom"
 import { useOutletContext } from "react-router-dom"
-import { getProduct } from '../services/product';
 export default function ProductDetail ()
 {
     const [product, setProduct] = useState({});
