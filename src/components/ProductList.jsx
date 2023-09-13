f7f020a86cc713a57c9277b5edb92f38da25bc44
MartinBournon <martinbournon@gmail.com>
2023-09-12 23:14:02 -0300

    eliminada etiqueta body

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,33 +1,31 @@
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-import {  useOutletContext } from 'react-router-dom';
+import { useOutletContext } from 'react-router-dom';
 export default function ProductList() {
 
     const products = useOutletContext();
     return (
+        <div>
+            <h1>Lista de Productos</h1>
+            <ul>
+                {products.map((product) => (
+                    <li key={product.id}>
 
-        <body>
-            <div>
-                <h1>Lista de Productos</h1>
-                <ul>
-                    {products.map((product) => (
-                        <li key={product.id}>
-
-                            <Card style={{ width: '18rem' }}>
-                                <Card.Img variant="top" src="holder.js/100px180" />
-                                <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
-                                    <Card.Text>
-                                        Some quick example text to build on the card title and make up the
-                                        bulk of the card's content.
-                                    </Card.Text>
-                                    <Button variant="primary">Go somewhere</Button>
-                                </Card.Body>
-                            </Card>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </body>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src="holder.js/100px180" />
+                            <Card.Body>
+                                <Card.Title>Card Title</Card.Title>
+                                <Card.Text>
+                                    {product.id}
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                </Card.Text>
+                                <Button variant="primary">Go somewhere</Button>
+                            </Card.Body>
+                        </Card>
+                    </li>
+                ))}
+            </ul>
+        </div>
     )
 }
