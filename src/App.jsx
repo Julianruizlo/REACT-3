83e6780ba9250e816f04b958814ac88ef08889f4
MartinBournon <martinbournon@gmail.com>
2023-09-12 23:05:40 -0300

    se arreglaron las rutas anidadas de productos

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProductList from './components/ProductList';
 import ProductDetail from './components/ProductDetail';
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.bundle.min.js"
 import './App.css'
+import ProductPage from './pages/ProductPage';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,12 +13,13 @@ function App() {
   return (
     <Router>
       <Routes>
-      <Route path="" element={<ProductList />} >
-        <Route path=":id" element={<ProductDetail />} />
-      </Route>
+        <Route path="products" element={<ProductPage />} >
+          <Route index element={<ProductList />} />
+          <Route path=":id" element={<ProductDetail />} />
+        </Route>
       </Routes>
     </Router>
-    )
+  )
 }
 
 export default App
